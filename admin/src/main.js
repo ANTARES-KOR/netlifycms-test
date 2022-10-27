a775698b88ee2c7c66944da84fa20c2ccda7732b
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2022-10-27 10:17:31 +0900

    script based setup

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,5 +1,5 @@
-import CMS from "netlify-cms-app";
-import { ko } from "netlify-cms-locales";
+// import CMS from "netlify-cms-app";
+// import { ko } from "netlify-cms-locales";
 // import cloudinary from "netlify-cms-media-library-cloudinary";
 import { YoutubeEmbedEditor } from "./youtube";
 
@@ -7,8 +7,10 @@ CMS.registerLocale("ko", ko);
 
 CMS.registerEditorComponent(YoutubeEmbedEditor);
 
-// CMS.registerMediaLibrary(cloudinary);
+CMS.registerMediaLibrary(cloudinary);
 CMS.registerPreviewStyle("./preview.css");
 
 // Initialize the CMS object
 CMS.init();
+
+// console.log("test");
